818b805e9f6fc7b81087942b8cd2abe8346cea38
Shahzod <shaydulovshahzod454@gmail.com>
2026-08-13 11:11:05 +0500

    Add JWT authentication

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -140,3 +140,9 @@ MAILERS = {
 }
 
 AUTH_USER_MODEL = 'accounts.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
