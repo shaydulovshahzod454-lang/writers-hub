973a04b45e865c8783f4a7a4714bc1c178cb7163
Shahzod <shaydulovshahzod454@gmail.com>
2026-08-16 09:34:31 +0500

    Read SECRET_KEY from environment variable

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ttpc3%-j^yz^jppm67@w2_cax)(i9a%z(f9p@y#w09w7jz_$u)'
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-ttpc3%-j^yz^jppm67@w2_cax)(i9a%z(f9p@y#w09w7jz_$u)')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
